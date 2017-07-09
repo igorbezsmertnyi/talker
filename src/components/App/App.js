8d3ef9cc22680d6b804294d0a42bcfec69c85d3c
Igor Bezsmertnyi <igorbessmertnij@Igors-MacBook-Pro.local>
2017-07-09 20:28:36 +0200

    refactor

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import './App.css'
+import style from './App.css'
 import Form from '../Form/Form.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={style.App}>
         <Form />
       </div>
     )
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import style from './Form.css'
+import throttle from 'throttle-debounce/throttle'
+import debounce from 'throttle-debounce/debounce'
 
 class Form extends Component {
   constructor(props) {
     super(props)
     this.state = { langs: undefined,
                    selectedLang: undefined,
-                   text: '' }
+                   text: '',
+                   isTalk: false }
     this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleTalk = this.handleTalk.bind(this)
     this.handleKeyUp = this.handleKeyUp.bind(this)
+    this.handleKeyDown = this.handleKeyDown.bind(this)
   }
 
   componentWillMount() {
@@ -18,38 +22,73 @@ class Form extends Component {
     }
   }
 
+  componentDidMount() {
+    window.addEventListener('keyup', e => {
+      if (e.keyCode >= 48 && e.keyCode <= 90 || e.keyCode == 8 || e.keyCode == 8) {
+        setTimeout(() => {
+          throttle(1, this.handleTalk(), false)
+          this.setState({ isTalk: false })
+        }, 1000)
+      }
+    })
+  }
+
   handleChange(e) {
     this.setState({ selectedLang: e.target.value })
+    this.textArea.focus()
+  }
+
+  handleKeyDown() {
+    this.setState({ isTalk: true })
   }
 
   handleKeyUp(e) {
-    this.setState({ text: e.target.value })
+    this.setState({text: e.target.value})
   }
 
-  handleSubmit(e) {
-    let msg = new SpeechSynthesisUtterance()
+  handleTalk() {
     const voices = window.speechSynthesis.getVoices()
-    msg.voice = voices[this.state.selectedLang]
-    msg.rate = 1
-    msg.pitch = 1
-    msg.text = this.state.text
+    let msg = new SpeechSynthesisUtterance()
 
-    speechSynthesis.speak(msg)
+    if (this.state.isTalk) {
+      setTimeout(() => {
+        msg.voice = voices[this.state.selectedLang]
+        msg.rate = 1
+        msg.pitch = 1
+        msg.text = this.state.text
+
+        speechSynthesis.speak(msg)
+
+        msg.onstart = e => {
+          this.setState({ isTalk: false })
+        }
+
+        msg.onend = e => {
+          this.setState({ isTalk: true })
+        }
+      }, 200)
+    }
   }
 
   render() {
     const langs = this.state.langs
 
     return(
-      <form className={style.form}>
-        <textarea onKeyUp={this.handleKeyUp} />
-        {langs && <select onChange={this.handleChange}>
-          {langs.map((lang, id) => {
-            return <option key={id} value={id}>{lang.name} - {lang.lang.slice(3, 5)}</option>
-          })}
-        </select>}
-        <input type="button" value="submit" onClick={this.handleSubmit} />
-      </form>
+      <div className={style.container}>
+        <form className={style.container__form}>
+          <textarea ref={(area) => this.textArea = area}
+                    className={style.container__form__textarea}
+                    onKeyDown={this.handleKeyDown}
+                    onKeyUp={this.handleKeyUp}
+                    autoFocus={true}
+                    placeholder="Welcome" />
+          {langs && <select onChange={this.handleChange}>
+            {langs.map((lang, id) => {
+              return <option key={id} value={id}>{lang.name} - {lang.lang.slice(3, 5)}</option>
+            })}
+          </select>}
+        </form>
+      </div>
     )
   }
 }
